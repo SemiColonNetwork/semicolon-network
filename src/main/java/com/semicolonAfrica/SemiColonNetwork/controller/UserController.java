007a478464b6528df19c0cfe65da08c1d926a832
Alawode Samuel <tolulope.alawode@gmail.com>
2023-09-11 14:48:31 +0100

    user

--- src/main/java/com/semicolonAfrica/SemiColonNetwork/controller/UserController.java
+++ src/main/java/com/semicolonAfrica/SemiColonNetwork/controller/UserController.java
@@ -3,6 +3,7 @@ package com.semicolonAfrica.SemiColonNetwork.controller;
 import com.semicolonAfrica.SemiColonNetwork.dtos.SaveUserRequest;
 import com.semicolonAfrica.SemiColonNetwork.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,9 @@ public class UserController {
 
     private UserService userService;
 
-    public String saveUser(@RequestBody SaveUserRequest saveUserRequest){
-        userService.saveUser(saveUserRequest);
+    @PostMapping("/createUser")
+    public String saveUser(){
+      //  userService.saveUser(saveUserRequest);
         return "User saved succesffully";
     }
 
