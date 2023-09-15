5d14b35b5ffa2ce500d48e96ffa272da1d2a88b9
SemiColonNetwork <144689807+SemiColonNetwork@users.noreply.github.com>
2023-09-15 01:23:08 +0100

    Update SaveUserRequest.java

--- src/main/java/com/semicolonAfrica/SemiColonNetwork/dtos/SaveUserRequest.java
+++ src/main/java/com/semicolonAfrica/SemiColonNetwork/dtos/SaveUserRequest.java
@@ -10,8 +10,7 @@ public class SaveUserRequest {
     private String emailAddress;
     private String phoneNumber;
     private String cohort;
-    private boolean isAncestor;
-    private boolean isNative;
+    private boolean isAncestorOrNative;
     private boolean isEmployed;
     private String techStack;
     private String language;
